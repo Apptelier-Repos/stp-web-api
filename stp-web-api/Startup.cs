f7b5b9f613f96bab0dd82b0f26d6f2cd54f2cc0e
Lesair Valmont <ixra.net@gmail.com>
2019-06-03 05:35:33 -0500

    [AB#37] Removing unnecessary using.
    And separating private field from constructor.

--- stp-web-api/Startup.cs
+++ stp-web-api/Startup.cs
@@ -1,21 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
 namespace stp_web_api
 {
     public class Startup
     {
         private readonly string MyAllowSpecificOrigin = "_myAllowSpecificOrigin";
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
