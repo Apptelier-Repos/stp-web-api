e2fbe0ceb0d7542e58aa7423f297566eff5531e0
Lesair Valmont <ixra.net@gmail.com>
2019-06-09 09:41:17 -0500

    [AB#88] User accounts controller.
    New  empty controller.

---
+++ stp-web-api/Controllers/UserAccountsController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace stp_web_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserAccountsController : ControllerBase
+    {
+    }
+}
